5cd8e2507a60a8de898589263aa44ecb0c5c0a78
dipanshuuk711 <dipanshuuk711@gmail.com>
2024-07-10 08:53:37 +0530

    Fixed Code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
-  const url = `https://food-del-backend-r691.onrender.com/`;
+  const url = `https://food-del-backend-r691.onrender.com`;
 
   return (
     <div>
